fa42da7f43c00f082a81c71824c890a72eaaaa5f
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2019-04-13 21:45:24 +0100

    Updates

---
+++ pages-manifest-4fa0987cb6ccdecb119b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{140:function(e){e.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{"dev-404-page-5f9":"386/path---dev-404-page-5-f-9-fab-0SUcWyAf8ecbYDsMhQkEfPzV8","header-3d7":"707/path---header-3-d-7-165-0SUcWyAf8ecbYDsMhQkEfPzV8",index:"140/path---index-6a9-0SUcWyAf8ecbYDsMhQkEfPzV8","index-1-264":"97/path---index-1-264-711-0SUcWyAf8ecbYDsMhQkEfPzV8"}}}}]);
+//# sourceMappingURL=pages-manifest-4fa0987cb6ccdecb119b.js.map
